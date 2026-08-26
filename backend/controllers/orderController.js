101e023bd31a74453882e865dabd6c59f6f35602
Anil Verma <anilmaherda2006@gmail.com>
2026-08-26 16:47:30 +0530

    almost done

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -10,11 +10,12 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 // placing user order for frontend
 const placeOrder = async (req, res) => {
 
-    const frontend_url = "http://localhost:5174"
+    const frontend_url = process.env.FRONTEND_URL;
 
     try {
+
         const newOrder = new orderModel({
-            userId: req.body.userId,
+            userId: req.userId,
             items: req.body.items,
             amount: req.body.amount,
             address: req.body.address
@@ -22,7 +23,10 @@ const placeOrder = async (req, res) => {
 
         await newOrder.save();
 
-        await userModel.findByIdAndUpdate(req.body.userId,{ cartData: {} });
+        await userModel.findByIdAndUpdate(
+            req.userId,
+            { cartData: {} }
+        );
 
         const line_items = req.body.items.map((item) => ({
             price_data: {
@@ -46,83 +50,87 @@ const placeOrder = async (req, res) => {
             quantity: 1
         });
 
-        const session = await stripe.checkout.sessions.create({
-            line_items: line_items,
-            mode: 'payment',
-            success_url: `${frontend_url}/verify?success=true&orderId=${newOrder._id}`,
-            cancel_url: `${frontend_url}/verify?success=false&orderId=${newOrder._id}`,
-            
-        });
-
-        res.json({success:true, session_url:session.url});
-
-
-    }
-    catch(e){
-        console.log(e);
-        res.json({success:false, message:"Error"})
-    }
-}
-
-const verifyOrder = async(req, res) =>{
-    const {orderId, success} = req.body;
-
-    try{
-        if(success==="true"){
-            await orderModel.findByIdAndUpdate(orderId, {payment:true});
-            res.json({success:true, message:"Paid"})
-        }
-        else{
-            await orderModel.findByIdAndDelete(orderId);
-            res.json({success:false, message:"Not Paid"})
-        }
-    }
-    catch(e){
-        console.log(e);
-        res.json({success:false, message:"Error"})
-    }
-}
-
-const userOrders = async(req, res) =>{
-    try{
-        const orders = await orderModel.find({userId:req.body.userId});
-        res.json({success:true, data:orders})
-    }
-    catch(e){
-        console.log(e);
-        res.json({success:false, message:"Error"});
-    }
-}
-
-// listing orders for admin panel
-const listOrders = async (req, res)=>{
-    try{
-        const orders = await orderModel.find({});
-        res.json({success:true, data:orders});
-    }
-    catch(e){
-        console.log(e);
-        res.json({success:false, message:"Error"});
-    }
-}
-
-const updateStatus = async (req, res) => {
-    try {
-        await orderModel.findByIdAndUpdate( req.body.orderId,{ status: req.body.status });
+        const session =
+            await stripe.checkout.sessions.create({
+                line_items,
+                mode: "payment",
+                success_url:
+                    `${frontend_url}/verify?success=true&orderId=${newOrder._id}`,
+                cancel_url:
+                    `${frontend_url}/verify?success=false&orderId=${newOrder._id}`,
+            });
 
         res.json({
             success: true,
-            message: "Updated status"
+            session_url: session.url
         });
-    }
-    catch(e) {
+
+    } catch (e) {
+
         console.log(e);
+
         res.json({
             success: false,
             message: "Error"
         });
     }
-}
+};
 
+const verifyOrder = async (req, res) => {
+  const { orderId, success } = req.body;
 
-export {placeOrder, verifyOrder, userOrders, listOrders, updateStatus};
+  try {
+    if (success === "true") {
+      await orderModel.findByIdAndUpdate(orderId, { payment: true });
+      res.json({ success: true, message: "Paid" });
+    } else {
+      await orderModel.findByIdAndDelete(orderId);
+      res.json({ success: false, message: "Not Paid" });
+    }
+  } catch (e) {
+    console.log(e);
+    res.json({ success: false, message: "Error" });
+  }
+};
+
+const userOrders = async (req, res) => {
+  try {
+    const orders = await orderModel.find({  userId: req.userId });
+    res.json({ success: true, data: orders });
+  } catch (e) {
+    console.log(e);
+    res.json({ success: false, message: "Error" });
+  }
+};
+
+// listing orders for admin panel
+const listOrders = async (req, res) => {
+  try {
+    const orders = await orderModel.find({});
+    res.json({ success: true, data: orders });
+  } catch (e) {
+    console.log(e);
+    res.json({ success: false, message: "Error" });
+  }
+};
+
+const updateStatus = async (req, res) => {
+  try {
+    await orderModel.findByIdAndUpdate(req.body.orderId, {
+      status: req.body.status,
+    });
+
+    res.json({
+      success: true,
+      message: "Updated status",
+    });
+  } catch (e) {
+    console.log(e);
+    res.json({
+      success: false,
+      message: "Error",
+    });
+  }
+};
+
+export { placeOrder, verifyOrder, userOrders, listOrders, updateStatus };
